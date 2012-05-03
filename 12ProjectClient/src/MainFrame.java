f9e81344e186974fe69d0004497d797f8418a956
wuminfajoy@gmail.com <wuminfajoy@gmail.com@5497137b-2e11-cacf-b768-d72713f0b2ca>
2012-05-03 02:30:35 +0000

    update

--- 12ProjectClient/src/MainFrame.java
+++ 12ProjectClient/src/MainFrame.java
@@ -9,8 +9,7 @@ import widgets.JugglerWidget;
 import widgets.RectangleWidget;
 import widgets.Widget;
 public class MainFrame extends JFrame{
-	private JPanel pnlFrame = new JPanel();
-	public ChatRoomClient server=null;
+	private JPanel contentPane = new JPanel();
 	public JButton btn1;
 	public JButton btn2;
 	public JButton btn3;
@@ -18,9 +17,9 @@ public class MainFrame extends JFrame{
 	public JButton selectBtn=null;
 	public JTextField txtField;
 	public JTextArea txtArea;
-	public JScrollPane scrollPane;
-	public JPanel pnlWhiteBorad ; 
-	public JPanel selectPnl=null;
+	public JScrollPane scrollPaneWhiteBorad;
+	public JPanel paneWhiteBorad ; 
+	public JPanel selectPane=null;
 	public int sX=0;
 	public int sY=0;
 	public Map<Widget,ChatPost> postObj=new LinkedHashMap<Widget,ChatPost>(); 
@@ -28,23 +27,23 @@ public class MainFrame extends JFrame{
 		setTitle("mainFrame");
 		setSize(750,600);
 		addWindowListener(windowAdapter);
-		add(pnlFrame);
-		pnlFrame.setLayout(null);
+		add(contentPane);
+		contentPane.setLayout(null);
 		
-		pnlWhiteBorad = new JPanel();
-		pnlWhiteBorad.setBounds(0, 0, 200, 200);
+		paneWhiteBorad = new JPanel();
+		paneWhiteBorad.setBounds(0, 0, 200, 200);
 		
-		pnlWhiteBorad.setLayout(null);
-		pnlWhiteBorad.setBackground(Color.white);
-		scrollPane=new JScrollPane(pnlWhiteBorad,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setBounds(0, 0, 500, 300);
+		paneWhiteBorad.setLayout(null);
+		paneWhiteBorad.setBackground(Color.white);
+		scrollPaneWhiteBorad=new JScrollPane(paneWhiteBorad,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPaneWhiteBorad.setBounds(0, 0, 500, 300);
 		
 		
-		pnlFrame.add(scrollPane);
+		contentPane.add(scrollPaneWhiteBorad);
 		
 		Panel pnl = new Panel();
 		pnl.setBounds(500, 0, 180, 300);
-		pnlFrame.add(pnl);
+		contentPane.add(pnl);
 		pnl.setLayout(new GridLayout(4, 0, 0, 0));
 		btn1 = new JButton("Rectangle");
 		pnl.add(btn1);		
@@ -55,47 +54,47 @@ public class MainFrame extends JFrame{
 		btn4 = new JButton("???");
 		pnl.add(btn4);
 		txtArea = new JTextArea(8,50);
-		JScrollPane scrollPane=new JScrollPane(txtArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setBounds(0, 306, 675, 175);
+		JScrollPane scrollPaneTxtArea=new JScrollPane(txtArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPaneTxtArea.setBounds(0, 306, 675, 175);
 		
-		pnlFrame.add(scrollPane);
+		contentPane.add(scrollPaneTxtArea);
 		
 		txtField = new JTextField();
 		txtField.setBounds(95, 487, 590, 23);
-		pnlFrame.add(txtField);
+		contentPane.add(txtField);
 		
 		Label label = new Label("Input");
 		label.setBounds(20, 487, 69, 23);
-		pnlFrame.add(label);
+		contentPane.add(label);
 		
 		SpringLayout sl_contentPane = new SpringLayout();
-		sl_contentPane.putConstraint(SpringLayout.WEST, this.scrollPane, 3, SpringLayout.WEST, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.EAST, this.scrollPane, 503, SpringLayout.WEST, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, this.scrollPane, 3, SpringLayout.NORTH, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, this.scrollPane, 303, SpringLayout.NORTH, pnlFrame);
+		sl_contentPane.putConstraint(SpringLayout.WEST, this.scrollPaneWhiteBorad, 3, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, this.scrollPaneWhiteBorad, 503, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, this.scrollPaneWhiteBorad, 3, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, this.scrollPaneWhiteBorad, 303, SpringLayout.NORTH, contentPane);
 		
 		
-		sl_contentPane.putConstraint(SpringLayout.WEST, pnl, 3, SpringLayout.EAST, this.scrollPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, pnl, -3, SpringLayout.EAST, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, pnl, 3, SpringLayout.NORTH, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, pnl, 0, SpringLayout.SOUTH, this.scrollPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, pnl, 3, SpringLayout.EAST, this.scrollPaneWhiteBorad);
+		sl_contentPane.putConstraint(SpringLayout.EAST, pnl, -3, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, pnl, 3, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, pnl, 0, SpringLayout.SOUTH, this.scrollPaneWhiteBorad);
 		
 		
-		sl_contentPane.putConstraint(SpringLayout.WEST, label, 3, SpringLayout.WEST, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.EAST, label, 53, SpringLayout.WEST, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, label,-33, SpringLayout.SOUTH, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, label,-3, SpringLayout.SOUTH, pnlFrame);
+		sl_contentPane.putConstraint(SpringLayout.WEST, label, 3, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, label, 53, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, label,-33, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, label,-3, SpringLayout.SOUTH, contentPane);
 		
 
 		sl_contentPane.putConstraint(SpringLayout.WEST, txtField, 0, SpringLayout.EAST, label);
-		sl_contentPane.putConstraint(SpringLayout.EAST, txtField, -3, SpringLayout.EAST, pnlFrame);
+		sl_contentPane.putConstraint(SpringLayout.EAST, txtField, -3, SpringLayout.EAST, contentPane);
 		sl_contentPane.putConstraint(SpringLayout.NORTH, txtField, 0, SpringLayout.NORTH, label);
 		sl_contentPane.putConstraint(SpringLayout.SOUTH, txtField, 0, SpringLayout.SOUTH, label);
 		
-		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPane, 3, SpringLayout.WEST, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPane, -3, SpringLayout.EAST, pnlFrame);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPane, 3, SpringLayout.SOUTH, this.scrollPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPane, -3, SpringLayout.NORTH, label);
+		sl_contentPane.putConstraint(SpringLayout.WEST, scrollPaneTxtArea, 3, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, scrollPaneTxtArea, -3, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, scrollPaneTxtArea, 3, SpringLayout.SOUTH, this.scrollPaneWhiteBorad);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, scrollPaneTxtArea, -3, SpringLayout.NORTH, label);
 		
 		
 		
@@ -103,7 +102,7 @@ public class MainFrame extends JFrame{
 		
 		
 		
-		pnlFrame.setLayout(sl_contentPane);
+		contentPane.setLayout(sl_contentPane);
 		
 	}
 	public MainFrame(){
@@ -112,7 +111,7 @@ public class MainFrame extends JFrame{
 		btn2.addActionListener(actionListener);
 		btn3.addActionListener(actionListener);
 		txtField.addKeyListener(keyAdapter);
-		pnlWhiteBorad.addMouseListener(mouseAdapter);
+		paneWhiteBorad.addMouseListener(mouseAdapter);
 	}
 
 	
@@ -191,13 +190,13 @@ public class MainFrame extends JFrame{
 	MouseAdapter mouseAdapter=new MouseAdapter() {
 		@Override
 		public void mousePressed(MouseEvent e) {
-			if(e.getComponent()!=pnlWhiteBorad){
-			selectPnl=(JPanel)e.getSource();
+			if(e.getComponent()!=paneWhiteBorad){
+			selectPane=(JPanel)e.getSource();
 			sX=e.getX();
 			sY=e.getY();
 			System.out.println("Pressed obj" );
 			}
-			if(e.getComponent()==pnlWhiteBorad){
+			if(e.getComponent()==paneWhiteBorad){
 				Widget w=null;
 				if(selectBtn==btn1){
 					w=new RectangleWidget();
@@ -228,16 +227,16 @@ public class MainFrame extends JFrame{
 		};
 		@Override
 		public void mouseReleased(MouseEvent e) {
-			if(selectPnl==null)
+			if(selectPane==null)
 				return;
-			int x= e.getXOnScreen()-selectPnl.getParent().getLocationOnScreen().x-sX;
-			int y= e.getYOnScreen()-selectPnl.getParent().getLocationOnScreen().y-sY;
+			int x= e.getXOnScreen()-selectPane.getParent().getLocationOnScreen().x-sX;
+			int y= e.getYOnScreen()-selectPane.getParent().getLocationOnScreen().y-sY;
 			if(x<0)x=0;
 			if(y<0)y=0;
-			ChatPost post=postObj.get(selectPnl);
+			ChatPost post=postObj.get(selectPane);
 			if(post==null){
 				//System.out.println("x:"+x+" y:"+y );
-				selectPnl.setLocation(x, y);
+				selectPane.setLocation(x, y);
 				pnlWhiteBoradResize();	
 				return;
 			}
@@ -246,20 +245,20 @@ public class MainFrame extends JFrame{
 			if(ChatClient.server.isLeave)
 				return;
 			ChatClient.server.invokeMove(post,x,y);
-			pnlWhiteBorad.scrollRectToVisible(selectPnl.getBounds());
-			selectPnl=null;
+			paneWhiteBorad.scrollRectToVisible(selectPane.getBounds());
+			selectPane=null;
 			System.out.println("Released obj");
-			scrollPane.revalidate();
+			scrollPaneWhiteBorad.revalidate();
 			//super.mouseReleased(e);
 		}
 
 		public void mouseDragged(MouseEvent e) {
-			if(selectPnl==e.getSource()){
-				int x= e.getXOnScreen()-selectPnl.getParent().getLocationOnScreen().x-sX;
-				int y= e.getYOnScreen()-selectPnl.getParent().getLocationOnScreen().y-sY;
+			if(selectPane==e.getSource()){
+				int x= e.getXOnScreen()-selectPane.getParent().getLocationOnScreen().x-sX;
+				int y= e.getYOnScreen()-selectPane.getParent().getLocationOnScreen().y-sY;
 				if(x<0)x=0;
 				if(y<0)y=0;
-				selectPnl.setLocation(x,y);
+				selectPane.setLocation(x,y);
 				pnlWhiteBoradResize();
 				/*
 				ChatPost post=postObj.get(selectPnl);
@@ -284,22 +283,19 @@ public class MainFrame extends JFrame{
 	void pnlWhiteBoradResize(){
 		double maxX=1;
 		double maxY=1;
-		for (int i=0;i<pnlWhiteBorad.getComponentCount();i++) {
-			JPanel p=(JPanel)pnlWhiteBorad.getComponent(i);
+		for (int i=0;i<paneWhiteBorad.getComponentCount();i++) {
+			JPanel p=(JPanel)paneWhiteBorad.getComponent(i);
 			if(maxX<p.getBounds().getMaxX())
 				maxX=p.getBounds().getMaxX();
 			if(maxY<p.getBounds().getMaxY())
 				maxY=p.getBounds().getMaxY();
 			
 		}
-		pnlWhiteBorad.setPreferredSize(new Dimension((int)maxX+6,(int)maxY+6));
-		pnlWhiteBorad.repaint();
-		scrollPane.revalidate();
+		paneWhiteBorad.setPreferredSize(new Dimension((int)maxX+6,(int)maxY+6));
+		paneWhiteBorad.repaint();
+		scrollPaneWhiteBorad.revalidate();
 	}
 	
-	public void beginLogin() throws InterruptedException{
-		server.beginLogin();
-	}
 	public void writeLine(String msg){
 		txtArea.append(msg);
 	   	txtArea.append("\n");
@@ -310,8 +306,8 @@ public class MainFrame extends JFrame{
 		
 	}
 	public void resetWhiteBorad(){
-		for (Component c : pnlWhiteBorad.getComponents()) {
-			pnlWhiteBorad.remove(c);
+		for (Component c : paneWhiteBorad.getComponents()) {
+			paneWhiteBorad.remove(c);
 			if(c instanceof Widget)
 				((Widget)c).destroy();
 		}		
@@ -322,7 +318,7 @@ public class MainFrame extends JFrame{
 		if(post.value instanceof Widget){
 			Widget w=(Widget)post.value;
 			postObj.put(w,post);
-			pnlWhiteBorad.add(w,0,0);
+			paneWhiteBorad.add(w,0,0);
 			pnlWhiteBoradResize();
 			if(!ChatClient.server.isLogin)
 				return;
@@ -345,7 +341,7 @@ public class MainFrame extends JFrame{
 	public void removeWidget(ChatPost post){
 		if(post.value instanceof Widget){
 			postObj.remove(post.value);
-			pnlWhiteBorad.remove((Widget)post.value);
+			paneWhiteBorad.remove((Widget)post.value);
 			pnlWhiteBoradResize();
 		}
 	}
